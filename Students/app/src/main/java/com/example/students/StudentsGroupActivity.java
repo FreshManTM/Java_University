84cb3546be2256c431baa27624bcb8bc44f1ab38
Fresh <freshman_tm@mail.ru>
2022-10-20 10:53:49 +0300

    LR4

---
+++ Students/app/src/main/java/com/example/students/StudentsGroupActivity.java
@@ -0,0 +1,14 @@
+package com.example.students;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StudentsGroupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_students_group);
+    }
+}
